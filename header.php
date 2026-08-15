80052f56e9c12cda54c7d0b5b07143f742545097
Toka <tokamoustafa44@gmail.com>
2026-08-15 23:47:22 +0300

    adding login and register pages

--- header.php
+++ header.php
@@ -10,14 +10,14 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./bootstrap/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./bootstrap/assets/css/style.css">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
 <body>
 
     <!-- Navbar -->
 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
 
             <a class="navbar-brand fw-bold fs-5 " href="#">
@@ -49,18 +49,18 @@
                     </li>
 
 
-                     <li class="nav-item">
+                    <li class="nav-item">
                         <a class="nav-link fw-medium fs-5" href="pricing.php">Pricing</a>
                     </li>
 
                 </ul>
 
                 <div>
-                    <a href="#" class="btn fw-medium fs-5 me-2" style="color: #262626;">
+                    <a href="login.php" class="btn fw-medium fs-5 me-2" style="color: #262626;">
                         Login
                     </a>
 
-                    <a href="#" class="btn text-white fs-5 fw-medium" style="background-color: #FF9500;">
+                    <a href="register.php" class="btn text-white fs-5 fw-medium" style="background-color: #FF9500;">
                         Register
                     </a>
                 </div>
@@ -69,5 +69,4 @@
 
         </div>
 
-        </nav>
-  
+    </nav>
