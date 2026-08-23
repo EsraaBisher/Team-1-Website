7c48758b619217b6d69b1c34df78d1d145b2a011
EsraaBisher <bshyrwad936@gmail.com>
2026-08-23 13:42:32 +0300

    Update project

--- header.php
+++ header.php
@@ -125,9 +125,9 @@ $userRole   = $_SESSION['role'] ?? null;
                     <!-- User is logged in -->
 
                     <div class="d-flex align-items-center gap-2">
-                        <a href="/Team-1-Website/<?php if ($userRole === 'teacher') echo 'bootstrap/assets/css/teachers/profil.php';
-                                                    elseif ($userRole === 'student') echo 'students/profile.php';
-                                                    else echo 'users/profile.php'; ?>" class=" d-flex align-items-center gap-2 text-decoration-none" style="color:#262626;">
+                        <a href="/Team-1-Website/<?php if ($userRole === 'teacher') echo 'teachers/dashboard.php';
+                            elseif ($userRole === 'student') echo 'students/profile.php';
+                            else echo 'users/profile.php'; ?>" class=" d-flex align-items-center gap-2 text-decoration-none" style="color:#262626;">
                             <img src="/Team-1-Website/<?= !empty($_SESSION['image']) ? $_SESSION['image'] : 'bootstrap/assets/image/avatar.webp' ?>"
                                 alt="profile" class="rounded-circle" style="width:36px;height:36px;object-fit:cover;">
                             <span class="fw-medium"><?= htmlspecialchars($_SESSION['name'] ?? 'Profile') ?></span>
